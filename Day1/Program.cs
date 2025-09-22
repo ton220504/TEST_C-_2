fd04f0fcd23aa872dc0643c5d217d49ba7d815d1
80011_Toan <vantoan.tran@spclt.com.vn>
2025-09-22 16:46:53 +0700

    next update 22/9

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -65,49 +65,117 @@
 //}
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Property (get, set, auto property)
+//public class NhanVien
+//{
+//    private string ten;
+//    private decimal luong;
+
+//    public string Ten
+//    {
+//        get { return ten; }
+//        set {
+//            if (!string.IsNullOrEmpty(value))
+//            {
+//                ten = value;
+//            }
+//        }
+//    }
+
+//    public decimal Luong
+//    {
+//        get { return luong; } 
+//        set
+//        {
+//            if (value >= 0) luong = value;
+//        }
+//    }
+
+//    public string Msnv { get; set; }
+
+//    public void HienThi()
+//    {
+//        Console.WriteLine($"MSNV: {Msnv}\nHo Ten: {Ten}\nLuong: {Luong}");
+//    }
+//}
+
+
+//public class Program
+//{
+//    public static void Main()
+//    {
+//        NhanVien nv1 = new NhanVien();
+//        nv1.Msnv = "1";
+//        nv1.Ten = "Toan";
+//        nv1.Luong = 15000000;
+
+//        nv1.HienThi();
+//    }
+//}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Kế thừa(Inheritance), Đa hình(Polymorphism)
+//      inheritance: Class con kế thừa class cha.
+//      virtual + override: cho phép ghi đè phương thức.
+//      abstract: bắt buộc class con phải override.
+//      interface: định nghĩa hành vi, class phải implement.
+
+//class cha
+
 public class NhanVien
 {
-    private string ten;
-    private decimal luong;
-
-    public string Ten
+    public string msnv { get; set; }
+    public string hoten { get; set; }
+    public decimal luongcoban { get; set; }
+    public virtual decimal Tinhluong()
     {
-        get { return ten; }
-        set {
-            if (!string.IsNullOrEmpty(value))
-            {
-                ten = value;
-            }
-        }
-    }
-
-    public decimal Luong
-    {
-        get { return luong; } 
-        set
-        {
-            if (value >= 0) luong = value;
-        }
-    }
-
-    public string Msnv { get; set; }
-
-    public void HienThi()
-    {
-        Console.WriteLine($"MSNV: {Msnv}\nHo Ten: {Ten}\nLuong: {Luong}");
+        return luongcoban;
     }
 }
 
+//class con kế thừa
+public class NhanVienPartTime : NhanVien
+{
+    public int Sogiolam { get; set; }
+    public decimal Luongtheogiolam { get; set; }
+
+    public override decimal Tinhluong()
+    {
+        return Sogiolam * Luongtheogiolam;
+    }
+}
 
 public class Program
 {
     public static void Main()
     {
-        NhanVien nv1 = new NhanVien();
-        nv1.Msnv = "1";
-        nv1.Ten = "Toan";
-        nv1.Luong = 15000000;
+        List<NhanVien> dsNhanVien = new List<NhanVien>();
 
-        nv1.HienThi();
+        //thêm nhân viên fulltime
+        NhanVien nv1 = new NhanVien { msnv = "ms01", hoten = "VanToan", luongcoban = 15000000 };
+        dsNhanVien.Add(nv1);
+
+        //thêm nhân viên part time
+        NhanVienPartTime nv2 = new NhanVienPartTime { msnv = "ms02", hoten = "VanA", Sogiolam = 8, Luongtheogiolam = 25000 };
+        dsNhanVien.Add(nv2);
+
+        Console.WriteLine("Danh sách nhân viên:");
+        foreach(var nv in dsNhanVien)
+        {
+            Console.WriteLine($"MSNV: {nv.msnv}, HoTen: {nv.hoten}, Luong: {nv.Tinhluong()}\n");
+        }
+
+        //tìm nhân viên
+        Console.Write("Nhap MSNV: ");
+        string ma = Console.ReadLine();
+        var tim = dsNhanVien.Find(n => n.msnv == ma);
+        if( tim != null)
+        {
+            Console.WriteLine($"MSNV: {tim.msnv}, HoTen: {tim.hoten}, Luong: {tim.Tinhluong()}");
+        }
+        else
+        {
+            Console.WriteLine("Khong tim thay NV theo MSNV");
+        }
     }
 }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ccae6c2495ba15d21356bb38aac236bb064ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
