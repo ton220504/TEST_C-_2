090b6dab794e411581375e0396fb122d005a5ef1
80011_Toan <vantoan.tran@spclt.com.vn>
2025-09-26 13:54:25 +0700

    add new ASP.NET core web app

---
+++ ASP.NET_MVC_Basic/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using ASP.NET_MVC_Basic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC_Basic.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private static List<Product> dsSanpham = new() {
+            new Product{MaSP=1, TenSP="iPhone 16", Gia=18500000},
+            new Product{MaSP=2, TenSP="iPhone 17", Gia=21500000}
+        };
+
+        //GET: product
+        public IActionResult Index()
+        {
+            return View(dsSanpham);
+        }
+
+        //GET: /product/create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //GET: /product/update
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var sp = dsSanpham.FirstOrDefault(p => p.MaSP == id);
+            if(sp == null) return NotFound();
+            return View(sp);
+        }
+        [HttpPost]
+        public IActionResult Update(Product pr)
+        {
+            var sp = dsSanpham.FirstOrDefault(p => p.MaSP == pr.MaSP);
+            if(sp == null) return NotFound();
+            if (ModelState.IsValid)
+            {
+                sp.TenSP = pr.TenSP;
+                sp.Gia = pr.Gia;
+                return RedirectToAction("Index");
+            }
+            return View(pr);
+        }
+
+        //POST: /product/create
+        [HttpPost]
+        public IActionResult Create(Product pr)
+        {
+            if (ModelState.IsValid)
+            {
+                pr.MaSP = dsSanpham.Count + 1;
+                dsSanpham.Add(pr);
+                return RedirectToAction("Index");
+            }
+            return View(pr);
+        }
+
+        //Xóa sản phẩm
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var sp = dsSanpham.FirstOrDefault(p => p.MaSP == id);
+            if (sp == null) return NotFound();
+            dsSanpham.Remove(sp);
+            return RedirectToAction("Index");
+        }
+
+       
+
+    }
+}
---
+++ ASP.NET_MVC_Basic/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_MVC_Basic.Models
+{
+    public class Product
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; } = string.Empty;
+        public decimal Gia { get; set; }
+    }
+}
